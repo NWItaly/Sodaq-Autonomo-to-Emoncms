00d41b0a1cb0d296dead366176d103fa041eb8f0
StefanoS <sacchetto@gmail.com>
2019-10-02 15:09:13 +0200

    Moved to Autonomo2Emoncms folder

--- Config.h
+++
@@ -1,9 +0,0 @@
-//Credentials
-#define SSID "<your WIFI name>"
-#define PASSWORD "<WIFI Password>"
-
-/* OPEN ENERGY MONITOR */
-const String myEmonData_serverIP = "<Host/IP of EmonCMS server>"; //IP server
-const PROGMEM unsigned int myEmonData_serverPort = 80; //IP Port
-const String myEmonData_apiKey = "<EmonCMS key to send data>";//api key cloud
-const PROGMEM byte myEmonData_node = 0;//identificate txShield (0=not send node number)
